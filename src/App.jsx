0a9cc47a66acf5bd2122cb8fcba5f26ea801d292
aliko <gracealiko09@gmail.com>
2025-03-23 18:24:11 +0000

    added error page with useRouteError to manage errors routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AppLayout from "./ui/AppLayout";
 import Home from "./ui/Home";
 import Menu from "./features/menu/menu";
+import Error from "./ui/Error";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
 
     children: [
       {
