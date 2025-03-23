cc419f1f87977fcf24fdafd86bce450fcfe6b665
aliko <gracealiko09@gmail.com>
2025-03-23 17:31:58 +0000

    added the restaurant api from spoonucular api

---
+++ src/services/apiRestaurant.js
@@ -0,0 +1,24 @@
+const API_URL = import.meta.env.VITE_API_URL;
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+export async function getMenu() {
+  const query = "dessert";
+  const res = await fetch(`${API_URL}?query=${query}&apiKey=${API_KEY}`);
+
+  if (!res.ok) throw new Error("Failed getting menu");
+
+  const data = await res.json();
+  console.log("API Response:", data);
+
+  if (!data.results || !Array.isArray(data.results)) {
+    throw new Error("Unexpected data structure, 'results' is not an array.");
+  }
+
+  const foodItems = data.resulsts.map((item) => ({
+    name: item.title,
+    imageUrl: item.image,
+    ingredients: item.usedIngredients.map((ingredient) => ingredient.name),
+  }));
+
+  return foodItems;
+}
